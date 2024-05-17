bd423fe0ce441fe17cbe083b83d90d8d7476a19a
Shiv Jindal <shivjindal.dda@gmail.com>
2024-05-17 09:25:51 +0530

    first commit of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IntroComponent } from './intro/intro.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-hello',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule,NavbarComponent, IntroComponent, CoursesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
   title = 'hello-courses';
+  
+  isActive = false;
+
+  isSubsribed = true;
+
+  email: string= 'nguser@gmail.com';
+
+  registeredUsers = ['Prerna', 'Arti', 'Dorji', 'Tahar', 'Sidik', 'Alex'];
+
+  subscribe() {
+
+    console.log('Thanks for subscribing.');
+    this.notifyUpdates(true);
+
+  }
+
+  notifyUpdates(subscribed:boolean) {
+
+    if(subscribed) {
+
+      console.log('You will be notified on new events.');
+
+    } else {
+
+      console.log('You will need to subscribe to be notified on events.');
+    }
+
+  
+  }
+
 }
